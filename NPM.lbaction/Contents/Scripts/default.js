08b583069645ffc983c63b8ca574b097d1dd9bc1
Aijun Li <liaijun.cn@gmail.com>
2021-02-09 13:47:26 +0800

    Update action for NPM

--- NPM.lbaction/Contents/Scripts/default.js
+++ NPM.lbaction/Contents/Scripts/default.js
@@ -5,7 +5,27 @@ function run() {
 }
 
 function runWithString(string) {
-  LaunchBar.openURL(
-    'https://www.npmjs.com/package/' + encodeURIComponent(string)
+  if (string === '') {
+    return []
+  }
+
+  let response = JSON.parse(
+    HTTP.get(
+      `https://www.npmjs.com/search/suggestions?q=${encodeURIComponent(string)}`
+    ).data
   )
+  let suggestions = []
+
+  for (let key in response) {
+    suggestions.push({
+      title: response[key].name,
+      icon: 'npm-icon.png',
+      badge: response[key].version,
+      alwaysShowsSubtitle: true,
+      subtitle: response[key].description,
+      url: response[key].links.npm
+    })
+  }
+
+  return suggestions
 }
